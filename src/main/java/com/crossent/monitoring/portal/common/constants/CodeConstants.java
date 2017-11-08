e297e3cb9b820a65267961a2fad33afb3c72841a
hangyeol <hgyeoljeong@crossent.com>
2017-11-08 13:44:35 +0900

    사용자 그룹 사용자 선택 추가

---
+++ src/main/java/com/crossent/monitoring/portal/common/constants/CodeConstants.java
@@ -0,0 +1,9 @@
+package com.crossent.monitoring.portal.common.constants;
+
+public class CodeConstants {
+
+    public static final String MG_GROUP_MANAGER="0000300001"; // static CodeConstants 재생성않고 바로 사용가능, final 상수
+
+    public static final String MG_GROUP_OPERRATOR="0000300002";
+
+}
